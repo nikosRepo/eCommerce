c4dbadeab2cd4bf7eec68dc0a8769e8d466b3ae5
Nikos Repousis <nikosrep@gmail.com>
2019-12-17 08:56:45 +0200

    firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,35 @@ const config = {
   measurementId: "G-G6K9D6XDCM"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  //create new user if not exist in firebase
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
+//create new user if not exist in firebase
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
