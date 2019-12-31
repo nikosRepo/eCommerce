9555d1805173cb01a5fd209b105ac755abf7a881
Nikos Repousis <nikosrep@gmail.com>
2019-12-31 17:16:37 +0200

    update firestore and add spinner

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,7 +20,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const snapShot = await userRef.get();
 
-  //create new user if not exist in firebase
+  //------------create new user if not exist in firebase
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -40,7 +40,44 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-//create new user if not exist in firebase
+//------------create new user if not exist in firebase
+
+//------------create new collection items
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+//------------create new collection items
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const trasnformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return trasnformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 firebase.initializeApp(config);
 
